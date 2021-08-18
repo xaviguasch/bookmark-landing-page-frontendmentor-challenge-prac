afd125654dec08ee0374940a22fb002eb34fbb12
xaviguasch <xaviguasch@gmail.com>
2021-08-18 18:59:05 +0200

    implemented faq js functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const closeBtn = document.querySelector('.close-nav-btn')
 const openBtn = document.querySelector('.open-nav-btn')
 const nav = document.querySelector('.nav')
-
 const links = document.querySelectorAll('.nav a')
+
+const questions = document.querySelectorAll('.question-top')
 
 const closeNavModal = () => {
   nav.classList.remove('navigation-open')
@@ -12,9 +13,24 @@ const openNavModal = () => {
   nav.classList.add('navigation-open')
 }
 
+const showAnswer = (e) => {
+  if (e.target.parentNode.nextElementSibling.classList.contains('show')) {
+    e.target.parentNode.nextElementSibling.classList.remove('show')
+  } else {
+    questions.forEach((q) => {
+      q.nextElementSibling.classList.remove('show')
+    })
+    e.target.parentNode.nextElementSibling.classList.add('show')
+  }
+}
+
 closeBtn.addEventListener('click', closeNavModal)
 openBtn.addEventListener('click', openNavModal)
 
 links.forEach((link) => {
   link.addEventListener('click', closeNavModal)
+})
+
+questions.forEach((q) => {
+  q.addEventListener('click', showAnswer)
 })
